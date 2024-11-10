7d49cb45acbe99d58b6bab69d572f0710fdbc379
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2024-11-10 20:03:01 +0530

    Add files via upload

---
+++ Week9/Day1/LearnSwitchCase.java
@@ -0,0 +1,20 @@
+package week9.day1;
+
+public class LearnSwitchCase {
+	public static void main(String[] args) {
+		String browser="edge";
+		
+		switch(browser) {
+		
+		case "Chrome":
+		System.out.println("Chrome brower opened");
+		break;
+		case "Edge":
+		System.out.println("Edge browser opened");
+		break;
+		 default:
+		System.out.println("Any of the browser");
+		}
+		
+	}
+}
