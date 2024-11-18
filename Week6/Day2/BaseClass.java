2ebe85f31873e4f462abb5e6dcbe029a29393493
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2024-11-18 23:58:36 +0530

    Delete Week6 directory

--- Week6/Day2/BaseClass.java
+++
@@ -1,7 +0,0 @@
-package stepdefinition;
-
-import org.openqa.selenium.chrome.ChromeDriver;
-
-public class BaseClass {
-	public static ChromeDriver driver;
-}
--- Week6/Day2/RunnerClass.java
+++
@@ -1,9 +0,0 @@
-package runner;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features="src/main/java/features/CreateLead.feature" ,glue="stepdefinition", monochrome=true, publish=true)
-public class RunnerClass extends AbstractTestNGCucumberTests {
-
-}
